abdbe0878b5cf7220a71cfc46f501ec0167bcf95
Ommer <osimjee@canoga.com>
2016-02-27 02:26:23 -0800

    all testcases values formatted to dic - completed

--- mongobot/reader/robot_results_parser.py
+++ mongobot/reader/robot_results_parser.py
@@ -12,10 +12,6 @@ class RobotResultsParser(object):
         self.xml_file = xml_file
         self.test_run = ExecutionResult(xml_file, include_keywords=include_keywords)
 
-    
-    def xml_to_mongodb(self):
-        pass
-
 
     def _traverse_suites(self, suite):
         """ Recusivelly iterate through suites hierarchy""" 
@@ -67,9 +63,56 @@ class RobotResultsParser(object):
                      },
                   ]
 
-        suite_tests = suite.tests  
+        suite_tests = suite.tests 
+        #
+        # formatting all test cases in dic
+        #
+        tests = self._get_tests(suite)
+        for test in tests:
+          print self._parse_test(test)
+        
         return suite_doc, suite_tests
 
+    #
+    # get all tests from all suites
+    # return list of test cases
+    #
+    def _get_tests(self,suite,tests=[]):
+
+        if suite.tests:
+          tests += suite.tests
+
+        if not suite.tests:
+            [self._get_tests(s,tests) for s in suite.suites]
+
+        return tests
+
+    #
+    # return test in dictionary 
+    #
+    def _parse_test(self,test):
+        logger.info('Parsing test: %s' % test.name) 
+        
+        test_doc = {}
+        test_doc['doc']         = test.doc 
+        test_doc['elapsedtime'] = test.elapsedtime
+        test_doc['endtime']     = self._format_robot_timestamp(test.endtime).\
+                                  strftime('%Y-%m-%d %H:%M:%S')
+        test_doc['starttime'] = self._format_robot_timestamp(test.starttime).\
+                                  strftime('%Y-%m-%d %H:%M:%S')      
+        test_doc['tags']         = test.tags
+        test_doc['status']      = test.status
+        test_doc['test_id']     = test.id
+        test_doc['keywords']    = test.keywords
+        test_doc['message']     = test.message
+        test_doc['passed']      = test.passed
+        test_doc['critical']    = test.critical
+        test_doc['longname']    = test.longname
+        test_doc['name']        = test.name
+        test_doc['parent']      = test.parent
+        test_doc['timeout']     = test.timeout
+
+        return test_doc
 
     def _format_robot_timestamp(self, timestamp):
         return datetime.strptime(timestamp, '%Y%m%d %H:%M:%S.%f')
